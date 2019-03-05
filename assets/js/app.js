ba38a0f4281b3ee6d7a6a72d2d31af1d5be7e8a8
Alissa Powers <aapowers@uci.edu>
2019-03-04 18:04:43 -0800

    adds autosizing to textareas

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,11 +1,15 @@
+// scss imports
 import '../scss/app.scss';
 import '../scss/base.scss';
 import '../scss/list.scss';
+import '../scss/_fragments/alert.scss';
 import '../scss/_fragments/navbar.scss';
 import '../scss/_fragments/form.scss';
+// js imports
 import 'bootstrap';
+import autosize from 'autosize';
 
-
+// require jQuery
 let $ = require('jQuery');
 
 // This removes flash alerts after 4 seconds
@@ -16,3 +20,6 @@ $(document).ready(function () {
         });
     }, 4000 ); // 4 secs
 });
+
+// this autosizes all textareas
+autosize($('textarea'));
